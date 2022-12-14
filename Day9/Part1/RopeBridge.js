3aef98d60a55ff2d076baf591a7523325bf07b20
ojb <curylo.olga@gmail.com>
2022-12-14 12:33:14 +0000

    Day9 part1 solved

--- Day9/Part1/RopeBridge.js
+++ Day9/Part1/RopeBridge.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
-const data = fs.readFileSync("../test.txt", "utf8");
+const data = fs.readFileSync("../input.txt", "utf8");
 const arr = data.split("\n").map((a) => {
   const [direction, distance] = a.split(" ");
   //   const obj = {splited[0]: ""}
@@ -15,12 +15,14 @@ console.log(arr);
 const headPositions = [[0, 0]];
 const tailPositions = [[0, 0]];
 
-function newPosition(prevPosition, arr) {
-  let position = prevPosition;
-  [x, y] = position;
+function newPosition(prevHeadPosition, prevTailPosition, arr) {
+  let head = prevHeadPosition;
+  [x, y] = head;
+  let tail = prevTailPosition;
+  [v, z] = tail;
 
   [direction, distance] = arr;
-  function countNewPos([x, y], direction) {
+  function countNewHeadPos([x, y], direction) {
     switch (direction) {
       case "R":
         x += 1;
@@ -37,37 +39,77 @@ function newPosition(prevPosition, arr) {
     }
     return [x, y];
   }
-
-  [...Array(distance).keys()].forEach((_) => {
-    position = countNewPos(position, direction);
-    headPositions.push(position);
-  });
-}
-arr.forEach((a) => newPosition(headPositions.at(-1), a));
-// console.log(headPositions);
-function generateTailPos(arr1, arr2) {
-  let position = arr2;
-  //   [hX, hY] = arr1;
-  [tX, tY] = position;
-
-  function countTailPos([hX, hY], [tX, tY]) {
-    if (hX === tX) {
-      hY === tY + 1 ? (tY += 1) : hY === tY - 1 ? (tY -= 1) : {};
-    } else if (hY === tY) {
-      //   console.log("hey");
-      hX === tX + 1 ? (tX += 1) : hX === tX - 1 ? (tX += 1) : {};
+  function countNewTailPos([x, y], [v, z], direction) {
+    if (
+      (v === x || v === x + 1 || v === x - 1) &&
+      (y === z || y === z + 1 || y === z - 1)
+    ) {
+      v = v;
+      z = z;
+      return [v, z];
     }
-    // console.log([tX, tY]);
-    return [tX, tY];
+
+    switch (direction) {
+      case "R":
+        v === x ? (v += 1) : ((v += 1), (z = y));
+        break;
+      case "L":
+        v === x ? (v -= 1) : ((v -= 1), (z = y));
+
+        break;
+      case "U":
+        z === y ? (z += 1) : ((z += 1), (v = x));
+
+        break;
+      case "D":
+        z === y ? (z -= 1) : ((z -= 1), (v = x));
+        break;
+    }
+
+    return [v, z];
   }
-
-  arr1.forEach(([hX, hY]) => {
-    // console.log(hX, hY, tX, tY);
-    position = countTailPos([hX, hY], position);
-
-    tailPositions.push(position);
+  [...Array(distance).keys()].forEach((_) => {
+    head = countNewHeadPos(head, direction);
+    headPositions.push(head);
+    tail = countNewTailPos(head, tail, direction);
+    tailPositions.push(tail);
   });
 }
-generateTailPos(headPositions, tailPositions.at(-1));
+arr.forEach((a) => newPosition(headPositions.at(-1), tailPositions.at(-1), a));
+// // console.log(headPositions);
+// function generateTailPos(arr1, arr2) {
+//   let position = arr2;
+//   //   [hX, hY] = arr1;
+//   [tX, tY] = position;
+
+//   function countTailPos([hX, hY], [tX, tY]) {
+//     if (hX === tX) {
+//       hY === tY + 1 ? (tY += 1) : hY === tY - 1 ? (tY -= 1) : {};
+//     } else if (hY === tY) {
+//       //   console.log("hey");
+//       hX === tX + 1 ? (tX += 1) : hX === tX - 1 ? (tX += 1) : {};
+//     }
+//     // console.log([tX, tY]);
+//     return [tX, tY];
+//   }
+
+//   arr1.forEach(([hX, hY]) => {
+//     // console.log(hX, hY, tX, tY);
+//     position = countTailPos([hX, hY], position);
+
+//     tailPositions.push(position);
+//   });
+// }
+// generateTailPos(headPositions, tailPositions.at(-1));
 console.log(headPositions);
 console.log(tailPositions);
+
+//remove duplicates
+const uniqueTailPositions = Array.from(
+  new Set(tailPositions.map(JSON.stringify)),
+  JSON.parse
+);
+console.log(uniqueTailPositions.length);
+// const tailing = new Set([...tails]);
+// console.log(tailing);
+// console.log(tailPositions);
